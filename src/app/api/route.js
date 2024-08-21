b48ea8ee9cd815d06f436bba4da0069e7fb880d9
soongon <soongon@gmail.com>
2024-08-21 15:22:22 +0900

    db connection ok..

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,5 +1,12 @@
+import { ConnectDB } from "@/lib/db";
 import { NextResponse } from "next/server";
+
+
+const loadDB = async () => {
+    await ConnectDB();
+}
   
+loadDB();
 
 //  GET::/api 경로로 접근하면 실행되는 함수
 export async function GET() {
